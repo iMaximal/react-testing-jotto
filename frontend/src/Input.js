20ba977c7118519be710e3105c3194759f431c97
Maksim Golitsyn <maxim@infofree.ru>
2018-10-19 22:35:23 +0500

    Add Redux props tests to Input

--- frontend/src/Input.js
+++ frontend/src/Input.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { guessWord } from './actions';
+
 export class Input extends Component {
 
     render() {
@@ -34,5 +36,5 @@ const mapStateToProps = ({ success }) => {
     return { success };
 };
 
-export default connect(mapStateToProps)(Input);
+export default connect(mapStateToProps, { guessWord })(Input);
 
