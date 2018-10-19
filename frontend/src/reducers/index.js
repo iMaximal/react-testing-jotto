e24511933bba30f1ac361ed7fa01df043bc3e62d
Maksim Golitsyn <maxim@infofree.ru>
2018-10-19 15:35:13 +0500

    Set up guessWords tests

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import success from './success.reducer';
+import guessedWords from './guessedWords.reducer';
 
 export default combineReducers({
     success,
+    guessedWords,
 });
