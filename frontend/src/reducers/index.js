42efcb8690c830a13c65944d7f4e1c19f7e7e486
Maksim Golitsyn <maxim@infofree.ru>
2018-10-19 16:23:32 +0500

    Added integration tests for store

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import success from './success.reducer';
 import guessedWords from './guessedWords.reducer';
+import secretWord from './secretWord.reducer';
 
 export default combineReducers({
     success,
     guessedWords,
+    secretWord,
 });
